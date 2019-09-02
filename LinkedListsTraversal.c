826a983f907b2926993c5b469394575f3652e2e9
codιng вloĸe <500060811@stu.upes.ac.in>
2019-09-03 04:15:04 +0530

    Add files via upload

---
+++ LinkedListsTraversal.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+void printlist(struct node *n)
+{
+    while(n != NULL) {
+        printf("%d\n", n->data);
+        n = n->next;
+    }
+}
+int main()
+{
+    //Initializing pointers to null
+    struct node* head = NULL;
+    struct node* second = NULL;
+    struct node* third = NULL;
+    //Allocating memory to the pointers , here type of pointer is the node pointer
+    //is the struct value
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third = (struct node*)malloc(sizeof(struct node));
+    head->data = 1;
+    head->next = second;
+
+    second->data = 3;
+    second-> next = third;
+
+    third-> data = 5;
+    third ->next = NULL;
+    printlist(head);
+}
